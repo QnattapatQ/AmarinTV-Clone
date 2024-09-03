d00e6857677fd4b8046c505aacf1bb6d3845715d
Nattapat_BookZ <nattapatbook2078@gmail.com>
2024-09-04 00:34:43 +0700

    update

--- src/components/SpotlightNews/SpotlightNews.jsx
+++ src/components/SpotlightNews/SpotlightNews.jsx
@@ -45,17 +45,21 @@ const SpotlightNews = () => {
                         <button className='bg-gradient-to-r from-[#143496] to-[#5677de] text-white py-2 px-[30px] rounded'>ดูทั้งหมด</button>
                     </div>
                 </div>
-               <div className='flex gap-4 bg-red-500'>
-                    <div className='basis-[66.67%] grid grid-cols-4 gap-4'>
+               <div className='flex max-992:flex-col gap-4'>
+                    <div className='basis-[66.67%] max-992:basis-full grid grid-cols-4 max-992:grid-cols-2 gap-4 pr-4 border-r border-[rgba(112,112,112,0.4)] max-992:border-0'>
                         {newsContent?.map((news, index) => (
-                            <div className={`${news.newsDesc !== "" ? 'col-span-4' : ''}`} key={index}>
+                            <div className={`${news.newsDesc !== "" ? 'col-span-4 max-992:col-span-2' : ''}`} key={index}>
                                 <div className={`${news.newsDesc !== "" ? 'flex flex-row-reverse' : ''} gap-4`}>
                                     <div className={`${news.newsDesc !== "" ? 'basis-1/2' : ''} h-full rounded-md overflow-hidden`}>
                                         <img className='h-full' src={news.newsCover} alt="" /> 
                                     </div>
                                     <div className={`${news.newsDesc !== "" ? 'basis-1/2' : ''} relative`}>
                                         <div className='relative'>
-                                            <p className={`${news.newsDesc === "" ? 'my-[10px]' : ''} text-[13px] text-[#143496] font-medium before:absolute before:left-0 before:top-1/2 before:w-[12px] before:h-[2px] before:bg-[#e1d732] before:block pl-4`}>{news.newsTag}</p>
+                                            <div className={`${news.newsDesc === "" ? 'my-[10px]' : ''} text-[13px] text-[#143496] font-medium before:absolute before:left-0 before:top-1/2 before:w-[12px] before:h-[2px] before:bg-[#e1d732] before:block pl-4`}>
+                                                <a href="#">
+                                                    {news.newsTag}
+                                                </a>
+                                            </div>
                                         </div>
                                         <div>
                                             <div className='my-2'>
@@ -73,21 +77,37 @@ const SpotlightNews = () => {
                             </div>
                         ))}
                     </div>
-                    <div className='basis-[33.33%] bg-blue-500 h-full'>
+                    <div className='basis-[33.33%] max-992:basis-full h-full'>
                         <div className='flex justify-between flex-col h-full'>
-                            <div>
-                                <h1 className='text-[22px]'>วีดีโอ</h1>
+                            <div className='mb-[5px]'>
+                                <h1 className='text-[22px] text-[#143496] font-semibold'>วีดีโอ</h1>
                             </div>
                             <div className='flex flex-col justify-between h-full'>
                                 {newsVideo?.map((news, index) => (
-                                    <div className='py-1' key={index}>
-                                        <div className='flex gap-3'>
-                                            <div className='basis-1/2'>
-                                                <img src={news.newsCover} alt="" />
+                                    <div className={`
+                                            ${index === 0 ? 'pt-2 pb-[20px]' : ''}
+                                            ${index === 1 ? 'py-[20px] border-y border-[rgba(112,112,112,0.4)]' : ''}
+                                            ${index === 2 ? 'pb-2 pt-[20px]' : ''}
+                                        `} key={index}>
+                                        <div className='grid grid-cols-2 max-992:grid-cols-[135px_1fr] gap-3'>
+                                            <div className='rounded-md overflow-hidden'>
+                                                <a href='#' className=''>
+                                                    <img src={news.newsCover} alt="" />
+                                                </a>
                                             </div>
-                                            <div className='basis-1/2'>
-                                                <div>
-                                                    <h3 className='line-clamp-2'>{news.newsTopic}</h3>
+                                            <div className=''>
+                                                <div className='relative'>
+                                                    <div className='text-[13px] text-[#143496] font-medium before:absolute before:left-0 before:top-1/2 before:w-[12px] before:h-[2px] before:bg-[#e1d732] before:block pl-4'>
+                                                        <a href="#">
+                                                            {news.newsTag}
+                                                        </a>
+                                                    </div>
+                                                </div>  
+                                                <div className=''>
+                                                    <div>
+                                                        <h3 className='line-clamp-2 text-[16px] font-medium my-2'>{news.newsTopic}</h3>
+                                                        <p className='mt-[8px] text-xs text-[#212121]'>{news.newsDate}</p>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
