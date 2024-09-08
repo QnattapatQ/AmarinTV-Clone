24bb9f722077e22ae0ac3099a1f1df34878ebbc0
QnattapatQ <nattapatbook2078@gmail.com>
2024-09-08 18:18:12 +0700

    update

--- src/components/OlympicNews/OlympicNews.jsx
+++ src/components/OlympicNews/OlympicNews.jsx
@@ -1,27 +1,32 @@
 import React from 'react'
 import bg_olympic from '../../assets/OlympicNews/bg_olympic.png';
 import logo_paralympic from '../../assets/OlympicNews/logo_paralympic.svg';
+import bg_tablet from '../../assets/OlympicNews/bg-tablet.png';
 import gold from '../../assets/OlympicNews/gold.png';
 import silver from '../../assets/OlympicNews/silver.png';
 import copper from '../../assets/OlympicNews/copper.png';
+import flag from '../../assets/OlympicNews/flag_paralympic.png';
 import olympicNews from './olympicNews.js';
 import winnerTitle from './winner.js';
 
 const OlympicNews = () => {
     return (
-        <div className='relative py-10 px-[15px]'>
-            <div className='absolute size-full top-0 px left-0 -z-30'>
+        <div className='relative py-10 px-[15px] overflow-hidden'>
+            <div className='absolute size-full top-0 left-0 -z-30 max-992:hidden max-768:block'>
                 <img className='size-full object-cover' src={bg_olympic} alt="" />
             </div>
+            <div className='hidden max-992:absolute -z-30 max-992:block max-992:size-full max-992:top-0 max-992:left-0 max-992:bottom-0 max-768:hidden'>
+                <img className='size-full object-cover object-top' src={bg_tablet} alt="" />
+            </div>
             <div className='max-w-[1170px] max-1200:max-w-[970px] max-992:max-w-[750px] w-full mx-auto'>
-                <div className='grid grid-cols-[1fr_380px] gap-[30px] h-full'>
+                <div className='grid grid-cols-[1fr_380px] max-992:grid-cols-1 gap-[30px] h-full'>
                     <div className=''>
                         <div className=''>
                             <img src={logo_paralympic} alt="" />
                         </div>
                         <div>
                             <h2 className='text-2xl text-center font-bold mb-[10px] mt-[20px]'>ข่าวพาราลิมปิก 2024</h2>
-                            <div className='bg-white rounded-md p-4 grid grid-cols-2 gap-x-6 gap-y-2'>
+                            <div className='bg-white rounded-md p-4 grid grid-cols-2 max-768:grid-cols-1 gap-x-6 gap-y-2'>
                                 {olympicNews.map((news, index) => (
                                     <div className={`
                                         ${index === 0 ? 'row-span-4' : 'border-b border-[#00000050]'}
@@ -50,7 +55,10 @@ const OlympicNews = () => {
                             </a>
                         </div>
                     </div>
-                    <div className=''>
+                    <div className='relative'>
+                        <div className='absolute -z-30 max-w-[65%] left-[-60%] top-[-1%] w-fit max-1200:max-w-[60%] max-1200:left-[-50%] max-1200:top-0 max-992:hidden max-768:block max-768:max-w-[45%] max-768:top-[-25%] max-768:right-[-12%] max-768:left-auto'>
+                            <img className='' src={flag} alt="" />
+                        </div>
                         <div>
                             <h3 className='bg-[#30155E] py-[10px] text-3xl rounded-lg text-white text-center font-semibold mb-[5px]'>สรุปอันดับเหรียญ</h3>
                             <ul>
@@ -68,7 +76,7 @@ const OlympicNews = () => {
                                     <div className='w-[15%] flex items-center justify-center text-sm font-medium'>รวม</div>
                                 </li>
                                 {winnerTitle.map((coin, index) => (
-                                     <li className={`${coin.country === "ไทย" ? "bg-[#550FB6] text-white" : ""} flex odd:bg-[#dbdbdb] bg-white mt-[2px] rounded-[4px]`} key={index}>
+                                     <li className={`${coin.country === "ไทย" ? "bg-[#550FB6] text-white" : "odd:bg-[#dbdbdb] bg-white"} flex mt-[2px] rounded-[4px]`} key={index}>
                                         <div className='w-[40%] flex items-center text-xs py-[10px] px-[5px]'>
                                             <span className='size-[30px] flex items-center justify-center'>{index + 1}</span>
                                             <p>{coin.country}</p>
