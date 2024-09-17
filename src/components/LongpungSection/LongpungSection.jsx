85cec695925bc49a135d740aed96197207f6bfff
Nattapat_BookZ <nattapatbook2078@gmail.com>
2024-09-18 00:47:47 +0700

    update

--- src/components/LongpungSection/LongpungSection.jsx
+++ src/components/LongpungSection/LongpungSection.jsx
@@ -18,9 +18,9 @@ const LongpungSection = () => {
                     </div>
                 </div>
                 <div className='mt-6'>
-                    <div className=''>
-                        {recommendFood.map((food) => (
-                            <a href='#' className='flex bg-white rounded-md overflow-hidden relative'>
+                    <div className='mb-[20px]'>
+                        {recommendFood.map((food, index) => (
+                            <a href='#' className='flex bg-white rounded-md overflow-hidden relative' key={index}>
                                 <div className='w-1/2 pl-[85px] py-[30px] pr-[15px]'>
                                     <div className='flex items-center'>
                                         <FaMapMarkerAlt className='text-[#ea3516] text-[11px] mr-[5px]'/>
@@ -38,6 +38,53 @@ const LongpungSection = () => {
                                 </div>
                             </a>
                         ))}
+                    </div>
+                    <div className='flex gap-[30px] mt-[40px]'>
+                        <div className='w-[58.33%] bg-[#1e1e1e] rounded-md px-[30px] pb-[5px] pt-[25px]'>
+                            <h3 className='text-white text-center mx-auto mb-[30px]'>
+                                <a className='text-[26px] border-b-2 border-b-[#cc4027] font-semibold' href="#">บทความ</a>
+                            </h3>
+                            <div className='flex gap-[30px]'>
+                                {articleStore.map((data, index) => (
+                                    <a href='#' className='bg-white rounded-md overflow-hidden mb-[20px]' key={index}>
+                                        <div>
+                                            <img src={data.articleImage} alt="" />
+                                        </div>
+                                        <div className='py-[5px] px-[10px]'>
+                                            <h3 className='line-clamp-1 my-[5px] font-medium text-base h-[25px]'>{data.articleTopic}</h3>
+                                            <p className='line-clamp-2 text-[13px] text-[#212121] mb-[5px] font-light'>{data.articleDesc}</p>
+                                            <p className='text-xs text-[#212121] py-2 font-light'>{data.date}</p>
+                                        </div>
+                                    </a>
+                                ))}
+                            </div>
+                        </div>
+                        <div className='w-[41.66%] bg-[#1e1e1e] rounded-md px-[30px] pb-[5px] pt-[25px]'>
+                            <h3 className='text-white text-center mx-auto mb-[30px]'>
+                                <a className='text-[26px] border-b-2 border-b-[#cc4027] font-semibold' href="#">ร้านแนะนำประจำเดือน</a>
+                            </h3>
+                            <div className='flex flex-col gap-[20px]'>
+                                {recommendStore.map((data, index) => (
+                                    <a href="#" className='bg-white rounded-md overflow-hidden'>
+                                        <div className='grid grid-cols-[200px_1fr]'>
+                                            <div>
+                                                <img src={data.foodImage} alt="" />
+                                            </div>
+                                            <div className='px-[10px] py-[5px] flex flex-col justify-evenly'>
+                                                <div className='flex items-center'>
+                                                    <FaMapMarkerAlt className='text-[#ea3516] text-[11px] mr-[5px]'/>
+                                                    <span className='text-[#666] text-[11px]'>{data.location}</span>
+                                                </div>
+                                                <h3 className='line-clamp-1 font-semibold my-[5px]'>
+                                                    {data.storeName}
+                                                </h3>
+                                                <p className='text-xs text-[#212121] font-light'>{data.date}</p>
+                                            </div>
+                                        </div>
+                                    </a>
+                                ))}
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
