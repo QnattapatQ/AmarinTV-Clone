6195178827f1dd1fc38c5150370b3312cce89faf
QnattapatQ <nattapatbook2078@gmail.com>
2024-09-20 18:00:49 +0700

    update

--- src/components/PopularNews/PopularNews.jsx
+++ src/components/PopularNews/PopularNews.jsx
@@ -1,4 +1,13 @@
 import React from 'react'
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Navigation } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import popularNews from './popularNews.js';
+
 
 const PopularNews = () => {
     return (
@@ -8,6 +17,33 @@ const PopularNews = () => {
                     <div className='after:absolute after:border-b-[6px] after:rounded-sm after:right-0 after:left-0 after:bottom-[-3px] after:w-[145px] after:max-768:w-[100px] after:border-[#ea3516] after:max-768:border-b-[3px] after:max-768:bottom-0'>
                         <h2 className='text-[38px] font-semibold max-768:text-2xl'>ข่าวยอดนิยม</h2>
                     </div>
+                </div>
+                <div className='mt-[30px] relative w-full'>
+                    <Swiper
+                         navigation={{
+                            nextEl: '.swiper-next-btn',
+                            prevEl: '.swiper-prev-btn',
+                            clickable: true,
+                        }}
+                        modules={[Navigation, Autoplay]}
+                        slidesPerView={3}
+                        spaceBetween={30}
+                        loop={true}
+                        autoplay={{
+                            delay: 10000,
+                        }}
+                    >   
+                        {popularNews.map((news, index) => (
+                                <SwiperSlide key={index}>
+                                    <div>
+                                        <img src={news.newsImage} alt="" />
+                                    </div>
+                                </SwiperSlide>
+                            ))
+                        }
+                    </Swiper>
+                    <div className='swiper-prev-btn absolute left-[2%] size-[38px] top-1/2 bg-white z-[10] scale-[1.6] rounded-full text-black text-center cursor-pointer'><FiChevronLeft/></div>
+                    <div className='swiper-next-btn absolute right-[2%] size-[38px] top-1/2 bg-white z-[10] scale-[1.6] rounded-full text-black text-center cursor-pointer'><FiChevronRight/></div>
                 </div>
             </div>
         </div>
---
+++ src/components/PopularNews/popularNews.js
@@ -0,0 +1,35 @@
+import popular_news_1 from '../../assets/PopularNews/popular_news_1.png';
+import popular_news_2 from '../../assets/PopularNews/popular_news_2.png';
+import popular_news_3 from '../../assets/PopularNews/popular_news_3.png';
+import popular_news_4 from '../../assets/PopularNews/popular_news_4.png';
+import popular_news_5 from '../../assets/PopularNews/popular_news_5.png';
+
+const popularNews = [
+    {
+        newsImage: popular_news_1,
+        newsTopic: "ดราม่า #แบนแน็กชาลี ชาวเน็ตขุดหลักฐานแฉยับ พูดย้อนแย้ง-ส่งรูปแบล็กเมลฝ่ายหญิง",
+        date: "18 ก.ย. 67"
+    },
+    {
+        newsImage: popular_news_2,
+        newsTopic: 'เปิดสถิติ "ฟุตซอลทีมชาติไทย" ปะทะ บราซิล ฟุตซอลโลก2024',
+        date: "19 ก.ย. 67"
+    },
+    {
+        newsImage: popular_news_3,
+        newsTopic: "ซิ่งรถเบนซ์หรูพลิกคว่ำ 2 ร่างกระเด็นดับสยองคาที่",
+        date: "19 ก.ย. 67"
+    },
+    {
+        newsImage: popular_news_4,
+        newsTopic: "ผู้ใหญ่บ้านสุดทน! ดำเนินคดีเพจดังกล่าวหาเป็นผู้ใหญ่บ้านมาจากพม่า",
+        date: "19 ก.ย. 67"
+    },
+    {
+        newsImage: popular_news_5,
+        newsTopic: '“พระพยอม” ชี้ พระออกแจกของช่วยน้ำท่วมไม่ผิด',
+        date: "19 ก.ย. 67"
+    },
+];
+
+export default popularNews
