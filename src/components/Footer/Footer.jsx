d459c1e25f44404cc9379f090508699f17a66743
Nattapat_BookZ <nattapatbook2078@gmail.com>
2024-09-29 00:57:35 +0700

    update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -34,6 +34,60 @@ const Footer = () => {
                             </a>
                         </div>
                     </div>
+                    <div className='grid grid-cols-3 mt-[50px]'>
+                        <div className='flex justify-between bg-red-500'>
+                            <div className='flex flex-col w-full gap-2 relative'>
+                                <h4>
+                                    <a href="#">ข่าว</a>
+                                </h4>
+                                <h4>
+                                    <a href="#">SPOYLIGHT</a>
+                                </h4>
+                                <h4>
+                                    <a href="#">วีดีโอ</a>
+                                </h4>
+                                <h4>
+                                    <a href="#">ละคร</a>
+                                </h4>
+                                <h4>
+                                    <a href="#">ชีพจรลงพุงไกด์</a>
+                                </h4>
+                                <div className='mt-[50px]'>
+                                    <img src="https://static.amarintv.com/assets/icon/sonp.svg" alt="" />
+                                </div>
+                                <div className='absolute bottom-[-20px] left-[205px]'>
+                                    <h5 className='text-xs font-semibold text-nowrap'>ดาวน์โหลด 34 HD แอป </h5>
+                                    <div className='flex gap-4'>
+                                        <img className='w-[82px] h-auto' src="https://static.amarintv.com/assets/images/icons/ios-download.png?v=0.01" alt="" />
+                                        <img className='w-[82px] h-auto' src="https://static.amarintv.com/assets/images/icons/android-download.png?v=0.01" alt="" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='flex flex-col gap-2 w-full'>
+                                <h4>
+                                    <a href="#">ไลฟ์สไตล์</a>
+                                </h4>
+                                <h4>
+                                    <a href="#">ยานยนต์</a>
+                                </h4>
+                                <h4>
+                                    <a href="#">ผังรายการ</a>
+                                </h4>
+                                <h4>
+                                    <a href="#">ตรวจหวย</a>
+                                </h4>
+                                <h4>
+                                    <a href="#">ผู้ประกาศข่าว</a>
+                                </h4>
+                            </div>
+                        </div>
+                        <div className='bg-green-500'>
+
+                        </div>
+                        <div className='bg-orange-500'>
+
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
